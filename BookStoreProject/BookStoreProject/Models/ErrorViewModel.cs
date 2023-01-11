515119d7d2a7043b8e6256399b98e1fff97d16ff
Özenç Durur <ozencdurur96@gmail.com>
2023-01-11 22:55:10 +0300

    update

--- BookStoreProject/BookStoreProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStoreProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
